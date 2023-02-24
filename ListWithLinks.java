dc7847d3eda99ba57afe0bc1a318d67a1468b3b7
Dmitri Stanchevici <dmitristanchevici@Dmitris-iMac.local>
2023-02-24 12:12:36 -0500

    1.16

---
+++ ListWithLinks.java
@@ -0,0 +1,67 @@
+class ListItem {
+
+    String data;
+    ListItem next;
+}
+
+public class ListWithLinks {
+
+    ListItem front = null;
+    ListItem rear = null;
+
+    int numItems = 0;
+
+    public void add (String s)
+    {
+	if (front == null) {
+	    front = new ListItem ();
+	    front.data = s;
+	    rear = front;
+	    rear.next = null;
+	}
+	else {
+	    ListItem nextOne = new ListItem ();
+	    nextOne.data = s;
+	    rear.next = nextOne;
+	    rear = nextOne;
+	}
+
+	numItems ++;
+    }
+
+    public int size ()
+    {
+	return numItems;
+    }
+
+    public String get (int i)
+    {
+	if (i >= numItems || i < 0) {
+	    return null;
+	}
+
+	ListItem listPtr = front;
+	for (int j=1; j<=i; j++) {
+	    listPtr = listPtr.next;
+	}
+
+	return listPtr.data;
+    }
+
+    public boolean contains (String s)
+    {
+	if (front == null) {
+	    return false;
+	}
+
+	ListItem listPtr = front;
+	while (listPtr != null) {
+	    if ( listPtr.data.equals(s) ) {
+		return true;
+	    }
+	    listPtr = listPtr.next;
+	}
+	return false;
+    }
+
+}
